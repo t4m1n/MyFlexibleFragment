49322e33df36ce34775f277e297332e047b5ec31
Zulfiqar Tamin <fiqartamin@gmail.com>
2019-08-23 10:32:01 +0700

    MainActivity menampilkan HomeFragment , hapus komen onCreateView

--- app/src/main/java/com/fiqartamin/myflexiblefragment/HomeFragment.java
+++ app/src/main/java/com/fiqartamin/myflexiblefragment/HomeFragment.java
@@ -21,14 +21,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-    /*
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
-    */
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
