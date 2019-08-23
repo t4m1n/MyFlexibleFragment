48e66c9d9111b03612363b7555427637053fe909
Zulfiqar Tamin <fiqartamin@gmail.com>
2019-08-23 15:14:23 +0700

    Dialog Fragment

--- app/src/main/java/com/fiqartamin/myflexiblefragment/DetailCategoryFragment.java
+++ app/src/main/java/com/fiqartamin/myflexiblefragment/DetailCategoryFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -72,7 +74,18 @@ public class DetailCategoryFragment extends Fragment implements View.OnClickList
             case R.id.btn_profile:
                 break;
             case R.id.btn_show_dialog:
+                OptionDialogFragment mOptionDialogFragment = new OptionDialogFragment();
+
+                FragmentManager mFragmentManager = getChildFragmentManager();
+                mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment.class.getSimpleName());
                 break;
         }
     }
+
+    OptionDialogFragment.OnOptionDialogListener optionDialogListener = new OptionDialogFragment.OnOptionDialogListener() {
+        @Override
+        public void onOptionChosen(String text) {
+            Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
